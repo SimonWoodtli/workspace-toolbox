c513b6d145adc2c630c7bce9477ee835931b2ec5
SimonWoodtli <xnasero@posteo.net>
2023-05-01 11:09:22 -0500

    💚 add flag to install nix non-interactive

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -uex pipefail
 apt-get update && apt-get -y upgrade
 
 ## install nix package manager:
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
 aptCount=$(yq '.apt[]' < /tmp/recipe.yml | wc -l)
 aptPackages=($(yq '.apt[]' < /tmp/recipe.yml))
