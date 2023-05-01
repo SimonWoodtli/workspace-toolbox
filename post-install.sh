87f7cfe7a081d1e070767ace78dcfa5f1c6d8056
SimonWoodtli <xnasero@posteo.net>
2023-05-01 10:11:25 -0500

    🔨 add build and post-install scripts

---
+++ post-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
