9e0cc4e598d9959c4f218c89f1a042f0a8a6254d
SimonWoodtli <xnasero@posteo.net>
2023-05-06 10:38:28 -0500

    💡 add comments

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+## manage host from within workspace
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
+## packages that I need but are not on ubuntu yet
