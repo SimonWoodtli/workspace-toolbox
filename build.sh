4531f617366f80343a12fcd699730ec6166e3e3a
SimonWoodtli <xnasero@posteo.net>
2023-05-18 12:27:06 -0500

    ➖📦 rm dotool

--- build.sh
+++ build.sh
@@ -15,7 +15,3 @@ cpan -I ${cpanPackages[@]}
 curl -LJ https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o /tmp/uplink_linux_amd64.zip
 unzip /tmp/uplink_linux_amd64.zip -d /usr/local/bin/
 chmod 755 /usr/local/bin/uplink
-
-git clone https://git.sr.ht/~geb/dotool /tmp/dotool
-cd /tmp/dotool
-./install.sh
